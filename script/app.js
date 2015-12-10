467b4ee711c570429d391b60c89ef9da03f3be21
lsinh <leichin.sinha@gmail.com>
2015-12-10 21:33:52 +0000

    exercise-2

--- script/app.js
+++ script/app.js
@@ -1,1 +1,23 @@
 // Write your code here!
+
+$(document).ready(function() {
+    
+    $('img').on('click', function () {
+        
+        // var img = '<img src='$(this).attr alt='Kitty #1';
+        
+        var img = '<img src="'+$(this).attr("src")+'"">';
+        var alt = '<p>"' + $(this).attr('alt') + '""</p>';
+        
+        $('body').append("<div class ='overlay'>" +img+alt + '</div>');
+        
+        //must append remove overlay inside callback function
+        $('.overlay').on('click', function () {
+            $(this).remove();
+        });
+        
+        
+    });
+    
+    
+})
