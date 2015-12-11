88d32b3419d8edd673d49e355b1ae9a2ef9cb3ad
lsinh <leichin.sinha@gmail.com>
2015-12-11 15:57:09 +0000

    exercise-4| ; git commit -m exercise-

--- script/app.js
+++ script/app.js
@@ -1,1 +1,41 @@
 // Write your code here!
+
+
+$(document).ready(function() {
+    // $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=montreal').then(
+    // function(res) {
+        
+    //     $('<div>"' + res.results[0].address_components[0].long_name + '"</div>').insertAfter($('h2'));
+        
+       
+    // }
+// );
+
+$.ajax({
+    
+    url: 'http://api.open-notify.org/iss-now.json',
+    dataType: 'jsonp',
+    success: function(dataFromJson) {
+        
+    }
+});
+
+    // $('<h3>programming</h3>').insertAfter($('h2'));
+    //blocked domain
+    
+});
+
+
+// $(document).ready(function() {
+
+// $.ajax({
+    
+//     url: 'http://api.open-notify.org/iss-now.json',
+//     dataType: 'jsonp',
+//     success: function(dataFromJson) {
+//         console.log(dataFromJson);
+//     }
+// });
+ 
+// });
+
